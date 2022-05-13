5595f2ba890d535607984d10987fc4445cfd65eb
PichunLEE <pc.lee.simplon@gmail.com>
2022-05-13 14:56:44 +0200

    updated Routes in HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use App\Repository\ImageRepository;
+use App\Repository\CommentaryRepository;
 
 class HomeController extends AbstractController
 {
@@ -15,4 +18,31 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function user(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/', name: 'app_image_index', methods: ['GET'])]
+    public function image(ImageRepository $imageRepository): Response
+    {
+        return $this->render('image/index.html.twig', [
+            'images' => $imageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/', name: 'app_commentary_index', methods: ['GET'])]
+    public function commentary(CommentaryRepository $commentaryRepository): Response
+    {
+        return $this->render('commentary/index.html.twig', [
+            'commentaries' => $commentaryRepository->findAll(),
+        ]);
+    }
+
+
+
 }
