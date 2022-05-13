344ab68250e22f37bfcba0bfa32dbcca5bdd779b
PichunLEE <pc.lee.simplon@gmail.com>
2022-05-13 16:46:06 +0200

    updated affichage d'img et path de chaque page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,37 +12,11 @@ use App\Repository\CommentaryRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ImageRepository $imageRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/', name: 'app_user_index', methods: ['GET'])]
-    public function user(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/', name: 'app_image_index', methods: ['GET'])]
-    public function image(ImageRepository $imageRepository): Response
-    {
-        return $this->render('image/index.html.twig', [
             'images' => $imageRepository->findAll(),
         ]);
     }
-
-    #[Route('/', name: 'app_commentary_index', methods: ['GET'])]
-    public function commentary(CommentaryRepository $commentaryRepository): Response
-    {
-        return $this->render('commentary/index.html.twig', [
-            'commentaries' => $commentaryRepository->findAll(),
-        ]);
-    }
-
-
-
 }
